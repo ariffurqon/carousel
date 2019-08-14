98e5f53976d35b387eb5a90726b9fbf348352a9c
Arif Furqon <arif.furqon@gmail.com>
2019-08-14 16:26:17 -0700

    customized create-react-app boilerplate
    :q
    :q

---
+++ src/photo-gallery.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './photo-gallery.css';
+
+class PhotoGallery extends React.Component{
+  render(){
+    return (
+      <div className="photo-gallery">
+        <h2>Hi Sexy!</h2>
+      </div>
+    );
+  }
+}
+
+export default PhotoGallery;
